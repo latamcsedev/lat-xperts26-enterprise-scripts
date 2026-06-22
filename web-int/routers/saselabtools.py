f3dd43df4ceebf84251be6c05e18e7cabb71a0c8
Erico Hortolan <erico.hortolan@gmail.com>
2026-06-22 19:35:15 -0300

    change script to restore sdwan vm to fix the static route, automatic install the policy packages and manually set fmg to workaround exception cases when dhcp discovery doesn't work

--- web-int/routers/saselabtools.py
+++ web-int/routers/saselabtools.py
@@ -67,6 +67,107 @@ def disable_offline_mode(host_ip,username,password):
         print(f"Unknown error on {host_ip}")
         return False
 
+def set_fmg(host_ip,username,password):
+    prompt = ".*[ ~]#.*"
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host_ip, username=username, password=password, timeout=10)
+        time.sleep(2)
+        interact = SSHClientInteraction(ssh, timeout=10, display=True)
+        interact.expect(prompt)
+        interact.send("config system central-management")
+        interact.expect(prompt)
+        interact.send("set type fortimanager")
+        interact.expect(prompt)
+        interact.send("set fmg 10.254.1.2")
+        interact.expect(prompt)
+        interact.send("end")
+        interact.expect(".*y/n.*")
+        interact.send("y")
+        interact.expect(".*y/n.*")
+        interact.send("y")
+        interact.expect(prompt)
+        time.sleep(2)
+        interact.send("show sys central-management")
+        interact.expect(prompt)
+        output = interact.current_output_clean
+        with open ("/root/tshoot.log", "a+") as f:
+            f.write(f"FMG registration result: {host_ip}\n")
+            f.write(f"{output}\n")
+        if not "serial-number" in output:
+            #failed to register on FMG, trying again
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"failed to register on FMG, trying again: {host_ip}\n")
+            try:
+                ssh = paramiko.SSHClient()
+                ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                ssh.connect(hostname=host_ip, username=username, password=password, timeout=10)
+                time.sleep(2)
+                interact = SSHClientInteraction(ssh, timeout=10, display=True)
+                interact.expect(prompt)
+                interact.send("config system central-management")
+                interact.expect(prompt)
+                interact.send("set type fortimanager")
+                interact.expect(prompt)
+                interact.send("set fmg 10.254.1.2")
+                interact.expect(prompt)
+                interact.send("end")
+                interact.expect(".*y/n.*")
+                interact.send("y")
+                interact.expect(".*y/n.*")
+                interact.send("y")
+                interact.expect(prompt)
+                time.sleep(2)
+                interact.send("show sys central-management")
+                interact.expect(prompt)
+                output = interact.current_output_clean
+                with open ("/root/tshoot.log", "a+") as f:
+                    f.write(f"FMG registration result: {host_ip}\n")
+                    f.write(f"{output}\n")
+                if not "serial-number" in output:
+                    return False
+                return True
+
+            except:
+                return False
+        return True
+    except:
+        print(f"Unknown error on {host_ip}")
+        #failed to register on FMG, trying again
+        with open ("/root/tshoot.log", "a+") as f:
+            f.write(f"failed to register on FMG, trying again: {host_ip}\n")
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=host_ip, username=username, password=password, timeout=10)
+            time.sleep(2)
+            interact = SSHClientInteraction(ssh, timeout=10, display=True)
+            interact.expect(prompt)
+            interact.send("config system central-management")
+            interact.expect(prompt)
+            interact.send("set type fortimanager")
+            interact.expect(prompt)
+            interact.send("set fmg 10.254.1.2")
+            interact.expect(prompt)
+            interact.send("end")
+            interact.expect(".*y/n.*")
+            interact.send("y")
+            interact.expect(".*y/n.*")
+            interact.send("y")
+            interact.expect(prompt)
+            time.sleep(2)
+            interact.send("show sys central-management")
+            interact.expect(prompt)
+            output = interact.current_output_clean
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"FMG registration result: {host_ip}\n")
+                f.write(f"{output}\n")
+            if not "serial-number" in output:
+                return False
+            return True
+        except:
+            return False
 
 def replace_sn_fmg():
     prompt = ".*[ ~]#.*"
@@ -77,7 +178,8 @@ def replace_sn_fmg():
     fmg_ip       = inventory.get("fmg_ip")
     fmg_user     = inventory.get("fmg_user")
     fmg_password = inventory.get("fmg_password")
-
+    with open ("/root/tshoot.log", "w+") as f:
+        f.write(f"Starting\n")
     for site_name, site_data in sites.items():
         
         ip = site_data.get("ip")
@@ -97,17 +199,25 @@ def replace_sn_fmg():
                 interact.expect(prompt)
             except Exception as e:
                 with open ("/root/tshoot.log", "a+") as f:
+                    f.write(f"Authentication failed on: {site_name}\n")
                     f.write(str(e))
+                    f.write(f"\n")
                 ssh = paramiko.SSHClient()
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                 ssh.connect(hostname=ip, username=fgt_user, password="", timeout=10)
                 time.sleep(2)
                 interact = SSHClientInteraction(ssh, timeout=10, display=True)
                 interact.expect('.*Password.*')
+                with open ("/root/tshoot.log", "a+") as f:
+                    f.write(f"{interact.current_output_clean}\n")
                 interact.send(fgt_password)
                 interact.expect('.*Password.*')
+                with open ("/root/tshoot.log", "a+") as f:
+                    f.write(f"{interact.current_output_clean}\n")
                 interact.send(fgt_password)
                 interact.expect(prompt)
+                with open ("/root/tshoot.log", "a+") as f:
+                    f.write(f"{interact.current_output_clean}\n")
                 ssh.close()
                 # Reconnect with new password
                 ssh = paramiko.SSHClient()
@@ -117,29 +227,37 @@ def replace_sn_fmg():
                 interact = SSHClientInteraction(ssh, timeout=10, display=True)
                 interact.expect(prompt)
             
-            interact.send("config system console")
-            interact.expect(prompt)
-            interact.send("set output standard")
-            interact.expect(prompt)
-            interact.send("end")
-            interact.expect(prompt)
-            interact.send("get system status")
+            interact.send("get system status | grep Serial")
             interact.expect(prompt)
             
             log = interact.current_output
             match = re.search(r"Serial-Number:\s+(.*)", log)
             real_sn = match.group(1).strip() if match else "Not Found"
-            
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"{real_sn}\n")
+            # Purge static route
+            interact.send("config router static")
+            interact.expect(prompt)
+            interact.send("purge")
+            interact.expect('.*y/n.*')
+            interact.send("y")
+            interact.expect(prompt)
+            interact.send("end")
+            interact.expect(prompt)
             # Send reset command
             interact.send("execute factoryreset2 keepvmlicense")
             interact.expect('.*y/n.*')
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"{interact.current_output_clean}\n")
             interact.send("y")
             ssh.close()
 
         except Exception as e:
             print(f"{e}")
             with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"Authentication failed on: {site_name}\n")
                 f.write(str(e))
+                f.write(f"\n")
             return False
         
         # Serial Number update on FMG
@@ -157,7 +275,7 @@ def replace_sn_fmg():
                 # Get what FMG currently has for this device
                 interact.send(f"diag dvm device list {real_sn}\n")
                 interact.expect(prompt)
-                if "Branch" in interact.current_output_clean or "Hub" in interact.current_output_clean:
+                if "fg1" in interact.current_output_clean or "fg2" in interact.current_output_clean:
                     print(f"{site_name}: sn {real_sn} already correct on FMG, skipped")
                 else:
                     interact.send(f"diag dvm device delete root {real_sn}")
@@ -169,6 +287,7 @@ def replace_sn_fmg():
                 print(f"Error during replacement: {str(e)}\n")
                 with open ("/root/tshoot.log", "a+") as f:
                     f.write(str(e))
+                    f.write(f"\n")
                 return False
 
             finally:
@@ -178,6 +297,7 @@ def replace_sn_fmg():
             print(f"Error during replacement: {str(e)}\n")
             with open ("/root/tshoot.log", "a+") as f:
                 f.write(str(e))
+                f.write(f"\n")
             return False
         
     return True
@@ -204,14 +324,147 @@ def restore_fmg_backup(fmg_ip, fmg_user, fmg_password, backup_name):
 
     print(f"Configuration restored\n")
 
+def fgt_online(host_ip,username,password):
+    prompt = ".*[ ~]#.*"
+    try:
+        time.sleep(0.5)
+        
+        try:
+            time.sleep(0.5)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=host_ip, username=username, password=password, timeout=10)
+            time.sleep(1)
+            interact = SSHClientInteraction(ssh, timeout=10, display=True)
+            interact.expect(prompt)
+        except Exception as e:
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"Authentication failed on: {host_ip}\n")
+                f.write(str(e))
+                f.write(f"\n")
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=host_ip, username=username, password="", timeout=10)
+            time.sleep(1)
+            interact = SSHClientInteraction(ssh, timeout=10, display=True)
+            interact.expect('.*Password.*')
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"{interact.current_output_clean}\n")
+            interact.send(password)
+            interact.expect('.*Password.*')
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"{interact.current_output_clean}\n")
+            interact.send(password)
+            interact.expect(prompt)
+            with open ("/root/tshoot.log", "a+") as f:
+                f.write(f"{interact.current_output_clean}\n")
+            ssh.close()
+            # Reconnect with new password
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=host_ip, username=username, password=password, timeout=10)
+            time.sleep(1)
+            interact = SSHClientInteraction(ssh, timeout=10, display=True)
+            interact.expect(prompt)
+            
+        interact.send("get system status | grep Serial")
+        interact.expect(prompt)
+        return True
+        
+    except:
+        return False
 
+def fmg_create_api_user(fmg_ip, fmg_user, fmg_password):
+    prompt = ".*[ ~]#.*"
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=fmg_ip, username=fmg_user, password=fmg_password, timeout=10)
+        time.sleep(2)
+        interact = SSHClientInteraction(ssh, timeout=10, display=True)
+        interact.expect(prompt)
+        interact.send("config system admin user")
+        interact.expect(prompt)
+        interact.send("edit admin-api")
+        interact.expect(prompt)
+        interact.send("set rpc-permit read-write")
+        interact.expect(prompt)
+        interact.send("set user_type api")
+        interact.expect(prompt)
+        interact.send("set password rRUp8!ndXerj5V")
+        interact.expect(prompt)
+        interact.send("set profileid Super_User")
+        interact.expect(prompt)
+        interact.send("end")
+        interact.expect(prompt)
+        ssh.close()
+    except:
+        print(f"Failed to create API user\n")
+    print(f"API user created\n")
 
+def fmg_install_policy(fmg_ip):
+    url = f"https://{fmg_ip}/jsonrpc"
+    headers = {"Content-Type": "application/json", "Authorization": "Bearer rRUp8!ndXerj5V"}
+    payload = {
+        "id": 1,
+        "method": "exec",
+        "params": [
+            {
+            "url": "/securityconsole/install/package",
+            "data": {
+                "adom": "root",
+                "pkg": "Policy-Package_Hubs",
+                "flags": [
+                "none"
+                ],
+                "scope": [
+                {
+                    "name": "HUBs"
+                }
+                ]
+            }
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=payload, verify=False)
+    with open ("/root/tshoot.log", "a+") as f:
+        f.write(f"{response.content}\n")
+
+    payload = {
+        "id": 1,
+        "method": "exec",
+        "params": [
+            {
+            "url": "/securityconsole/install/package",
+            "data": {
+                "adom": "root",
+                "pkg": "Policy-Package_Spokes",
+                "flags": [
+                "none"
+                ],
+                "scope": [
+                {
+                    "name": "SPKs"
+                }
+                ]
+            }
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=payload, verify=False)
+    with open ("/root/tshoot.log", "a+") as f:
+        f.write(f"{response.content}\n")
 
 async def prepare_sdwan_devices():
     inventory = load_inventory()
     fmg_ip       = inventory.get("fmg_ip")
     fmg_user     = inventory.get("fmg_user")
     fmg_password = inventory.get("fmg_password")
+    fgt_user = inventory.get("fgt_user")
+    fgt_password = inventory.get("fgt_password")
+    sites = inventory.get("sites", {})
 
     yield f"Preparing SD-WAN environment, please do not refresh or close this page\n"
     await asyncio.sleep(0.5)
@@ -258,26 +511,65 @@ async def prepare_sdwan_devices():
             await asyncio.sleep(10)
             if (device_online(fmg_ip, fmg_user, fmg_password, "A1_FortiManager")):
                 break
+        
         #check if FMG is back online
         if not device_online(fmg_ip, fmg_user, fmg_password, "A1_FortiManager"):
             yield f"Failed to check that FMG is back online, aborting\n"
             await asyncio.sleep(0.5)
             return
+        
+        #replace SN's
         yield f"Replacing FGT SNs on FMG\n"
         await asyncio.sleep(0.5)
-        #replace SN's
-
         if not replace_sn_fmg():
             yield f"Failed to replace SNs on FMG, aborting\n"
             await asyncio.sleep(0.5)
             return
+        
+        #disable offline mode
         yield f"Disabling offline mode on FMG\n"
         await asyncio.sleep(0.5)
-        #disable offline mode
         if not disable_offline_mode(fmg_ip, fmg_user, fmg_password):
             yield f"Failed to disable offline mode, aborting\n"
             await asyncio.sleep(0.5)
             return
+        
+        #force FMG on FGTs
+        yield f"Starting to prepare FGTs\n"
+        await asyncio.sleep(10)
+        for site_name, site_data in sites.items():
+            ip = site_data.get("ip")
+            for retry in range(0,30):
+                await asyncio.sleep(10)
+                if fgt_online(ip,fgt_user,fgt_password):
+                    set_fmg(ip,fgt_user,fgt_password)
+                    break
+                yield f"{site_name} not online yet (retry: {retry + 1})\n"
+
+        #Install policy package
+        for retry in range(0,30):
+            all_online = True
+            for site_name, site_data in sites.items():
+                ip = site_data.get("ip")
+                if not fgt_online(ip,fgt_user,fgt_password):
+                    all_online = False
+                    yield f"{site_name} not online yet (retry: {retry + 1})\n"
+                    await asyncio.sleep(0.5)
+            if all_online:
+                break
+            else:
+                await asyncio.sleep(10)
+        yield f"Starting to install the Policy Packages\n"
+        await asyncio.sleep(0.5)
+        with open ("/root/tshoot.log", "a+") as f:
+            f.write(f"Creating API user\n")
+        fmg_create_api_user(fmg_ip, fmg_user, fmg_password)
+        
+        with open ("/root/tshoot.log", "a+") as f:
+            f.write(f"Install Policy Package\n")
+        fmg_install_policy(fmg_ip)
+        yield f"Installation initiated, please check the status on FMG Task Monitor\n"
+        await asyncio.sleep(0.5)
         # Finished
         yield f"Script finished\n"
         await asyncio.sleep(0.5)
